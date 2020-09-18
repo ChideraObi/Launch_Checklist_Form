41daa847da1f8ffbef6811251712d46077e4e15e
Chidera Obinali <chideraobinali1@gmail.com>
2020-09-17 21:39:41 -0500

    First try again

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]"); 
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+
+
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+         
+      } else if (typeof pilotName !== 'string' || typeof copilotName !== 'string' || isNan(fuelLevel) || isNan(cargoMass)) {
+         alert("Form is invalid. Please try again."); 
+         event.preventDefault();
+
+      }
+
+   });
+});
+
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+<h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${}</li>
+   <li>Diameter: ${}</li>
+   <li>Star: ${}</li>
+   <li>Distance from Earth: ${}</li>
+   <li>Number of Moons: ${}</li>
+</ol>
+<img src="${}">
+*/
