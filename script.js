c719bb2c9eb40778bff5f0c7194f619c550a96be
Chidera Obinali <chideraobinali1@gmail.com>
2020-09-20 09:38:21 -0500

    JSON for Mission Destination is Fuctional

--- script.js
+++ script.js
@@ -24,6 +24,25 @@ window.addEventListener("load", function() {
       } else {
          faultyItems.style.visibility = "visible"; 
          
+         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+            //Accesses the linked JSON in response
+            response.json().then( function(json) {
+               const div = document.getElementById("missionTarget");
+            //Populates data into HTML fields 
+               div.innerHTML = `
+               <h2>Mission Destination</h2>
+               <ol>
+               <li>Name: ${json[2].name}</li>
+               <li>Diameter: ${json[2].diameter}</li>
+               <li>Star: ${json[2].star}</li>
+               <li>Distance from Earth: ${json[2].distance}</li>
+               <li>Number of Moons: ${json[2].moons}</li>
+               </ol>
+               <img src="${json[2].image}"> 
+               `;
+            });
+         });
+
          launchStatus.innerHTML = `Shuttle is Ready for Launch`;
          launchStatus.style.color = "green";
 
@@ -54,14 +73,3 @@ window.addEventListener("load", function() {
    });
 });
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
